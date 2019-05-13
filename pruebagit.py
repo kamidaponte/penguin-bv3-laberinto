4e232ff1337ee7904d9900c8d9da856dfc1a6f9b
kamidaponte <kamidaponte@gmail.com>
2019-05-13 12:45:32 -0400

    se agrego un nuevo archivo

--- pruebagit.py
+++ pruebagit.py
@@ -1,1 +1,18 @@
 bgdgdgdbgdbbdds
+#################
+#comandos de GIT
+#################
+
+#git add .
+#git commit -m "el manesaje"
+#git push                    (para empujar el codigo del local al remoto)
+#git pull                    (para estirar el codigo del remoto al local)
+#################
+#comandos para navegar en la terminal
+#################
+
+# ls o dir (para ver los documentos existentes)
+# cd nombre_de_archivo  (para entrar en el archivo)
+# cd .. (para retroceder de/la carpeta/archivo)
+
+
